a2b0c8d5bbfc4ce7c7b7a368e9e712a3ad37edf5
slargodiaz <sonder672@hotmail.com>
2026-02-03 21:45:10 -0500

    Cambio en librerías de telemetría az app insights

--- Logging/LogExtensions.cs
+++ Logging/LogExtensions.cs
@@ -1,13 +1,34 @@
-﻿namespace StressApi.Logging;
+﻿using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace StressApi.Logging;
 
 public static class LogExtensions
 {
     public static void ConfigureLogging(this WebApplicationBuilder builder)
     {
-        //builder.Logging.ClearProviders();
-        builder.Logging.AddConsole();
-        builder.Logging.AddDebug();
+        string serviceName = "StressApi";
+        string serviceVersion = "1.0.0";
+        
+        builder.Logging.AddOpenTelemetry(options =>
+        {
+            options
+                .SetResourceBuilder(
+                    ResourceBuilder.CreateDefault()
+                        .AddService(serviceName, serviceVersion: serviceVersion))
+                .AddConsoleExporter();
+        });
 
-        builder.Services.AddApplicationInsightsTelemetry();
+        builder.Services.AddOpenTelemetry()
+            .ConfigureResource(resource => resource.AddService(serviceName, serviceVersion: serviceVersion))
+            .WithTracing(tracing => tracing
+                .AddAspNetCoreInstrumentation()
+                .AddConsoleExporter())
+            .WithMetrics(metrics => metrics
+                .AddAspNetCoreInstrumentation()
+                .AddConsoleExporter());
     }
 }
